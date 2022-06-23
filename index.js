90c68bd600eba0f56f5be10da8249666d7a75c0a
Kiki <sulaimonrokiat1270@gmail.com>
2022-06-23 23:00:36 +0100

    javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+document.write("My name is Rokiat Sulaimon. I am 5'4ft tall. I am a Nigerian, born and bred.")
+
+alert("Name: Rokiat Sulaimon\nHeight: 5'4ft\nCountry: Nigeria")
+
+console.log("Name: Rokiat Sulaimon\nHeight: 5'4ft\nCountry: Nigeria")
